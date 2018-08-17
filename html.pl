a9883b37b74c7a413226f9103bdd929aebcc1132
tannakaken <tannakaken@gmail.com>
2018-08-18 01:35:45 +0900

    特殊効果を追加

--- html.pl
+++ html.pl
@@ -6,8 +6,10 @@ use Encode qw/decode encode/;
 sub mix {
   my $line = shift;
   while ($line =~ /\#\{(.*?)\}\{(.*?)\}/) {
+    my ($len_1, $len_2) = (length($1), length($2));
+    my $len = $len_1 > $len_2 ? $len_1 : $len_2;
     my $cases = <<EOS;
-<span class="overlap"><span class="first">$1</span><span class="second">$2</span></span>
+<span class="overlap" style="display : inline-block; width : ${len}em;"><span class="first">$1</span><span class="second">$2</span></span>
 EOS
     $line =~ s/\#\{.*?\}\{.*?\}/$cases/;
   }
@@ -19,8 +21,10 @@ my $text = decode('UTF-8', <<'EOS');
 <html lang="ja">
   <head>
     <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
     <title>悪夢</title>
     <style>
+
     body {
       background-color : #90ee90;
     }
@@ -47,6 +51,16 @@ my $text = decode('UTF-8', <<'EOS');
     p {
       text-indent : 1em;
     }
+    span.overlap {
+      text-indent : 0;
+    }
+    span.first {
+      position : absolute;
+    }
+    span.second {
+      opacity : 0.0;
+    }
+    
     </style>
   </head>
   <body>
@@ -54,6 +68,7 @@ my $text = decode('UTF-8', <<'EOS');
     <section id="one">
       <p>
 EOS
+
 my $new = 1;
 my $section = 2;
 my @numbers = qw/zero one two three four five size seven eight nine ten/;
@@ -89,6 +104,31 @@ EOS
 
 $text .= <<'EOS';
   </body>
+  <script>
+    window.addEventListener('load', function() {
+      let opacity = 1.0;
+      let d = -0.1;
+      let firsts = document.getElementsByClassName('first');
+      let seconds = document.getElementsByClassName('second');
+      let length = firsts.length;
+      setInterval(function() {
+        let new_opacity = opacity + d;
+        if (new_opacity <= 0.0) {
+          opacity = 0.0;
+          d = -d
+        } else if (new_opacity + d >= 1.0) {
+          opacity = 1.0
+          d = -d;
+        } else {
+          opacity = new_opacity;
+        }
+        for (let i = 0; i < length; i++) {
+            firsts[i].style.opacity = opacity.toString();
+            seconds[i].style.opacity = (1 - opacity).toString();
+        }
+      }, 100);
+    }, false);
+  </script>
 </html>
 EOS
 
